84bab761ec888e9933f7e15e52cda8191fe6c08d
Daniel Košč <xkosc@fi.muni.cz>
2020-10-13 00:02:10 +0200

    var_parser added

---
+++ libs/automated-problem-generation-lib/generator/var_parser.py
@@ -0,0 +1,16 @@
+from generator.var_object import Variable
+import yaml
+
+def parser_var_file(var_file):
+    var_list = yaml.load(var_file, Loader=yaml.FullLoader)
+    var_objects = []
+    for var in var_list.keys():
+        name = var
+        type = var_list[var][0]["type"]
+        min = var_list[var][0].get("min")
+        max = var_list[var][0].get("max")
+        prohibited = var_list[var][0].get("prohibited")
+        if prohibited == None:
+            prohibited = []
+        var_objects.append(Variable(name, type, min, max, prohibited))
+    return var_objects
