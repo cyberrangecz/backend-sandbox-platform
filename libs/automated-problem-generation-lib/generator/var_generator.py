b0cb2f97eb7a2b85d0496a916d09bcaafb2d965d
Daniel Košč <xkosc@fi.muni.cz>
2020-10-13 00:32:39 +0200

    added docstring

--- libs/automated-problem-generation-lib/generator/var_generator.py
+++ libs/automated-problem-generation-lib/generator/var_generator.py
@@ -2,7 +2,12 @@ import string, random, os
 
 name_file_path = "names.txt"
 
+'''
 
+return value - string one of the name in text file
+argument - file : path to file with names
+
+'''
 def get_random_name(name_file):
     l = []
     with open(name_file, "r") as f:
@@ -11,6 +16,13 @@ def get_random_name(name_file):
         return l[random.randint(0, len(l) - 1)]
 
 
+
+'''
+
+return value - int
+argument - var_obj : object type of Variable with possible restrictions
+
+'''
 def get_random_port(var_obj):
     min = var_obj.min
     max = var_obj.max
@@ -25,6 +37,12 @@ def get_random_port(var_obj):
             return str(port)
 
 
+'''
+
+return value - string IPv4
+argument - var_obj : object type of Variable with possible restrictions
+
+'''
 def get_random_IP(var_obj):
     octet_list_min = (var_obj.min or " ").split(".")
     octet_list_max = (var_obj.max or " ").split(".")
@@ -57,18 +75,35 @@ def get_random_IP(var_obj):
         if not ip in var_obj.prohibited:
             return ip[:-1]
 
+'''
 
+return value - string curent working directory
+argument - file : path to file
+
+
+'''
 def get_cwd(file):
     _ROOT = os.path.abspath(os.path.dirname(__file__))
     return os.path.join(_ROOT, file)
 
+'''
 
+return value - string random letters and digits with exact length
+argument - length : length of the password
+
+'''
 def get_random_password(length):
     letters_and_digits = string.ascii_letters + string.digits
     result_str = ''.join((random.choice(letters_and_digits) for i in range(length)))
     return result_str
 
+''''
+functions fill each Variable object's attribute generated_value from argument with generated value
 
+return value - list of Variable objects
+argument - variables : list of Variable objects
+
+'''
 def generate_randomized_arg(variables):
     for var in variables:
         if (var.type).lower() == 'username':
@@ -82,14 +117,24 @@ def generate_randomized_arg(variables):
             var.generated_value = get_random_IP(var)
     return variables
 
+'''
 
+return value - dictionary with name of the variable as key and generate value as value
+argument - variables : list of Variable objects
+
+'''
 def map_var_list_to_dict(var_list):
     var_dict = dict()
     for var in var_list:
         var_dict[var.name] = var.generated_value
     return var_dict
 
+'''
 
+return value - dictionary with name of the variable as key and generate value as value
+argument - variables : list of Variable objects
+
+'''
 def generate(variable_list):
     list_of_generated_objects = generate_randomized_arg(variable_list)
     return map_var_list_to_dict(list_of_generated_objects)
