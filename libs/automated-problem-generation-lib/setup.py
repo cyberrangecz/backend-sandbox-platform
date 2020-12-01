3ee298821109333ed2af632d99ab771df333cc3d
Daniel Košč <xkosc@fi.muni.cz>
2020-12-01 22:12:00 +0100

    added text.txt and removed delete function

--- libs/automated-problem-generation-lib/setup.py
+++ libs/automated-problem-generation-lib/setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="generator",
-    version="0.0.3",
+    version="0.0.4",
     author="Daniel Kosc",
     author_email="485652@mail.muni.cz",
     description="Random generator of selected variables",
