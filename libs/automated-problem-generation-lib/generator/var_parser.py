9cfa4b5c18488aff5b317e6c4a1c2ff73e8fa849
Daniel Košč <xkosc@fi.muni.cz>
2021-01-02 23:59:55 +0100

    if parser found error return None

--- libs/automated-problem-generation-lib/generator/var_parser.py
+++ libs/automated-problem-generation-lib/generator/var_parser.py
@@ -2,29 +2,18 @@ from generator.var_object import Variable
 import yaml
 
 def parser_var_file(var_file):
-    '''
-        Main function to parse variable attributes from input.
-
-        Parameters
-            ----------
-            variables : corresponding file object
-                file with input data required for generating name, type and optional restrictions
-
-        Returns
-            -------
-            list
-                list of Variable objects
-
-        '''
     var_list = yaml.load(var_file, Loader=yaml.FullLoader)
     var_objects = []
     for var in var_list.keys():
-        name = var
-        type = var_list[var][0]["type"]
-        min = var_list[var][0].get("min")
-        max = var_list[var][0].get("max")
-        prohibited = var_list[var][0].get("prohibited")
-        if prohibited == None:
-            prohibited = []
-        var_objects.append(Variable(name, type, min, max, prohibited))
+        try:
+            name = var
+            type = var_list[var][0]["type"]
+            min = var_list[var][0].get("min")
+            max = var_list[var][0].get("max")
+            prohibited = var_list[var][0].get("prohibited")
+            if prohibited == None:
+                prohibited = []
+            var_objects.append(Variable(name, type, min, max, prohibited))
+        except:
+            return None
     return var_objects
--- libs/automated-problem-generation-lib/setup.py
+++ libs/automated-problem-generation-lib/setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="generator",
-    version="0.0.5",
+    version="0.0.6",
     author="Daniel Kosc",
     author_email="485652@mail.muni.cz",
     description="Random generator of selected variables",
