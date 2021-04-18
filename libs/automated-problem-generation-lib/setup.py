53d9032bb38408f5669db98422d5d6e27ff5cb17
Daniel Kosc <da741kos@htwg-konstanz.de>
2021-04-18 15:31:15 +0200

    setup.py new version

--- libs/automated-problem-generation-lib/setup.py
+++ libs/automated-problem-generation-lib/setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="generator",
-    version="0.1.0",
+    version="0.1.1",
     author="Daniel Kosc",
     author_email="485652@mail.muni.cz",
     description="Random generator of selected variables",
