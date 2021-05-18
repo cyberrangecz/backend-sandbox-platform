51bafe3f5feacc20b89aeef14f73558729c7460d
Daniel Kosc <da741kos@htwg-konstanz.de>
2021-05-18 15:11:56 +0200

    fixed path in tests

--- libs/automated-problem-generation-lib/tests/unit_test.py
+++ libs/automated-problem-generation-lib/tests/unit_test.py
@@ -3,19 +3,20 @@ from unittest import TestCase
 from generator.var_parser import parser_var_file
 from generator.var_generator import generate
 
+
 class TryParser(TestCase):
     def test_run_parser(self):
-        with open(os.path.join("kosc-automated-problem-generation-lib","tests","variables.yml")) as file:
+        with open(os.path.join("tests", "variables.yml")) as file:
             parser_var_file(file)
         self.assertTrue(True)
 
     def test_return_not_None(self):
-        with open(os.path.join("kosc-automated-problem-generation-lib","tests","variables.yml")) as file:
+        with open(os.path.join("tests", "variables.yml")) as file:
             result = parser_var_file(file)
         self.assertTrue(result != None)
 
     def test_return_is_valid(self):
-        with open(os.path.join("kosc-automated-problem-generation-lib","tests","variables.yml")) as file:
+        with open(os.path.join("tests", "variables.yml")) as file:
             result = parser_var_file(file)
         self.assertTrue(result != None)
         self.assertTrue(len(result) == 5)
@@ -33,7 +34,7 @@ class TryParser(TestCase):
         self.assertTrue(result[1].min == None)
         self.assertTrue(result[1].max == None)
         self.assertTrue(result[1].length == None)
-        self.assertTrue(result[1].prohibited == [1,2,3])
+        self.assertTrue(result[1].prohibited == [1, 2, 3])
         self.assertTrue(result[1].generated_value == "")
 
         self.assertTrue(result[2].type == "password")
@@ -57,36 +58,36 @@ class TryParser(TestCase):
         self.assertTrue(result[4].min == 5)
         self.assertTrue(result[4].max == 10)
         self.assertTrue(result[4].length == None)
-        self.assertTrue(result[4].prohibited == [1,2,3,78,9])
+        self.assertTrue(result[4].prohibited == [1, 2, 3, 78, 9])
         self.assertTrue(result[4].generated_value == "")
 
     def test_run_parser_err(self):
-        with open(os.path.join("kosc-automated-problem-generation-lib","tests","variables_err.yml")) as file:
+        with open(os.path.join("tests", "variables_err.yml")) as file:
             parser_var_file(file)
         self.assertTrue(True)
 
     def test_return_not_None(self):
-        with open(os.path.join("kosc-automated-problem-generation-lib","tests","variables_err.yml")) as file:
+        with open(os.path.join("tests", "variables_err.yml")) as file:
             result = parser_var_file(file)
         self.assertTrue(result == None)
 
 
 class TryGenerator(TestCase):
     def test_return_not_None(self):
-        with open(os.path.join("kosc-automated-problem-generation-lib", "tests", "variables.yml")) as file:
+        with open(os.path.join("tests", "variables.yml")) as file:
             result = parser_var_file(file)
         self.assertTrue(result != None)
 
     def test_generate(self):
-        with open(os.path.join("kosc-automated-problem-generation-lib", "tests", "variables.yml")) as file:
+        with open(os.path.join("tests", "variables.yml")) as file:
             result = parser_var_file(file)
-            generate(result,1234)
+            generate(result, 1234)
         self.assertTrue(result != None)
 
     def test_generate_values(self):
-        with open(os.path.join("kosc-automated-problem-generation-lib", "tests", "variables.yml")) as file:
+        with open(os.path.join("tests", "variables.yml")) as file:
             result = parser_var_file(file)
-            generate(result,1234)
+            generate(result, 1234)
             print(result)
         self.assertTrue(result[0].generated_value.__contains__("I always did something"))
         self.assertTrue(result[1].generated_value == "38721")
