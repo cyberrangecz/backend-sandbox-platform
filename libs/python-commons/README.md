0521cb2ea965393aa0040e31a38d4051048be35d
Juraj Paluba <492988@mail.muni.cz>
2022-02-28 13:05:29 +0100

    create README, deploy and CI/CD operations

---
+++ libs/python-commons/README.md
@@ -0,0 +1,5 @@
+# KYPO Python Commons
+The repository for common classes of Python-base KYPO projects.
+
+## Common classes
+ * **cloud_commons** -- classes used by KYPO cloud clients and [terraform-client](https://gitlab.ics.muni.cz/muni-kypo-crp/backend-python/kypo-terraform-client)
---
+++ libs/python-commons/setup.py
@@ -0,0 +1,24 @@
+import os
+from setuptools import setup, find_namespace_packages
+
+
+# Utility function to read the README file.
+# Used for the long_description.  It's nice, because now 1) we have a top level
+# README file and 2) it's easier to type in the README file than to put a raw
+# string in below ...
+def read(filename):
+    return open(os.path.join(os.path.dirname(__file__), filename)).read()
+
+
+setup(
+    name='kypo-python-commons',
+    author='MASARYK UNIVERSITY',
+    description='KYPO Python Commons',
+    long_description=read('README.md'),
+    packages=find_namespace_packages(include=['kypo.*'], exclude=['tests']),
+    install_requires=[
+        'kypo-topology-definition==0.5.0',
+    ],
+    python_requires='>=3.8',
+    zip_safe=False
+)
