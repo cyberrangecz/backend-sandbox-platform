9dd23347c73625e546f9bc17a42f0e01c8604b54
Andrej Kiruscanka <kiruscanka@cshub.cz>
2025-02-14 12:56:41 +0100

    Fork and include CyberSecurity Hub, z.s. in the license
    Co-authored-by: Andrej Kiruscanka <kiruscanka@cshub.cz>
    Co-authored-by: Tomas Sapak <sapak@cshub.cz>

--- libs/automated-problem-generation-lib/setup.py
+++ libs/automated-problem-generation-lib/setup.py
@@ -4,14 +4,14 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="automated-problem-generation-lib",
+    name="crczp-automated-problem-generation-lib",
     author="Daniel Kosc",
     author_email="485652@mail.muni.cz",
     description="Random generator of selected variables",
     install_requires=['pyYAML', 'better_profanity'],
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://gitlab.fi.muni.cz/kypolab/theses/kosc-automated-problem-generation",
+    url="https://github.com/cyberrangecz/backend-automated-problem-generation-lib",
     packages=setuptools.find_namespace_packages(include=['generator'], exclude=['tests']),
     package_dir={'generator': './generator'},
     package_data={'generator': ['./*.txt', './*.yml']},
@@ -20,5 +20,5 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.6',
+    python_requires='>=3.12',
 )
